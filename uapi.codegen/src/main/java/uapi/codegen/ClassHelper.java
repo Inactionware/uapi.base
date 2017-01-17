f75b0752ec40fc6bd631b9d1909d503b89864f87
xquan <xiaoming.quan@netscout.com>
2017-01-17 11:20:49 +0800

    #9 Add ClassHelper to support uapi.service project

---
+++ uapi.codegen/src/main/java/uapi/codegen/ClassHelper.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright (C) 2017. The UAPI Authors
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at the LICENSE file.
+ *
+ * You must gained the permission from the authors if you want to
+ * use the project into a commercial product
+ */
+
+package uapi.codegen;
+
+import com.google.common.base.Strings;
+import uapi.InvalidArgumentException;
+import uapi.common.WordHelper;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+public final class ClassHelper {
+
+    private static final String FIELD_PREFIX    = "_";
+    private static final String SETTER_PREFIX   = "set";
+    private static final String ADD_PREFIX      = "add";
+    private static final String PUT_PREFIX      = "put";
+
+    public static String makeSetterName(String fieldName, boolean isCollection, boolean isMap) {
+        if (Strings.isNullOrEmpty(fieldName)) {
+            throw new IllegalArgumentException("The field name can't be empty or null");
+        }
+        String propName;
+        if (fieldName.startsWith(FIELD_PREFIX)) {
+            propName = fieldName.substring(1);
+        } else {
+            propName = fieldName;
+        }
+        String setterName;
+        if (isCollection) {
+            propName = WordHelper.singularize(propName);
+            setterName = ADD_PREFIX + propName.substring(0, 1).toUpperCase() + propName.substring(1, propName.length());
+        } else if (isMap) {
+            propName = WordHelper.singularize(propName);
+            setterName = PUT_PREFIX + propName.substring(0, 1).toUpperCase() + propName.substring(1, propName.length());
+        } else {
+            setterName = SETTER_PREFIX + propName.substring(0, 1).toUpperCase() + propName.substring(1, propName.length());
+        }
+        return setterName;
+    }
+
+//    public static Class<?> getElementType(Class<?> collectionClass, Type collectionType, ChangeableBoolean isCollection) {
+//        if (! (collectionType instanceof ParameterizedType)) {
+//            return collectionClass;
+//        }
+//        if (Collection.class.isAssignableFrom(collectionClass)) {
+//            isCollection.set(true);
+//            Type elemType = ((ParameterizedType) collectionType).getActualTypeArguments()[0];
+//            if (elemType instanceof ParameterizedType) {
+//                return getElementType((Class<?>) ((ParameterizedType) elemType).getRawType(), elemType, isCollection);
+//            } else {
+//                return (Class<?>) elemType;
+//            }
+//        } else if (Map.class.isAssignableFrom(collectionClass)) {
+//            isCollection.set(true);
+//            Type elemType = ((ParameterizedType) collectionType).getActualTypeArguments()[1];
+//            if (elemType instanceof ParameterizedType) {
+//                return getElementType((Class<?>) ((ParameterizedType) elemType).getRawType(), elemType, isCollection);
+//            } else {
+//                return (Class<?>) elemType;
+//            }
+//        } else {
+//            return collectionClass;
+//        }
+//    }
+
+    public static Class<?>[] getInterfaceParameterizedClasses(Class<?> type, Class<?> interfaceType) {
+        if (type == null) {
+            throw new InvalidArgumentException("type", InvalidArgumentException.InvalidArgumentType.EMPTY);
+        }
+        Class<?>[] paramClasses = null;
+        List<Type> intfTypes = Arrays.asList(type.getGenericInterfaces());
+        for (Type intfType : intfTypes) {
+            if (! (intfType instanceof ParameterizedType)) {
+                continue;
+            }
+            Type rowType = ((ParameterizedType) intfType).getRawType();
+            if (! rowType.equals(interfaceType)) {
+                continue;
+            }
+            Type[] paramTypes = ((ParameterizedType) intfType).getActualTypeArguments();
+            paramClasses = new Class<?>[paramTypes.length];
+            for (int i = 0; i < paramTypes.length; i++) {
+                paramClasses[i] = (Class<?>) paramTypes[i];
+            }
+        }
+        return paramClasses;
+    }
+}
---
+++ uapi.codegen/src/test/groovy/uapi/codegen/ClassHelperTest.groovy
@@ -0,0 +1,81 @@
+/*
+ * Copyright (C) 2017. The UAPI Authors
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at the LICENSE file.
+ *
+ * You must gained the permission from the authors if you want to
+ * use the project into a commercial product
+ */
+
+package uapi.codegen
+
+import spock.lang.Specification
+
+import java.lang.annotation.Annotation
+
+/**
+ * Unit test for ClassHelper
+ */
+class ClassHelperTest extends Specification {
+
+    def 'test CollectionField'() {
+        expect:
+        ClassHelper.makeSetterName(prop, isCollection, isMap) == setterName
+
+        where:
+        prop        | isCollection  | isMap | setterName
+        'messages'  | true          | false | 'addMessage'
+        'children'  | true          | false | 'addChild'
+    }
+
+    def  'test GetInterfaceParameterizedClasses'() {
+        given:
+        Class<?>[] paramTypes = ClassHelper.getInterfaceParameterizedClasses(FakeService.class, IFakeInterface.class);
+
+        expect:
+        paramTypes.length == 1
+        paramTypes[0] == TestAnno.class
+    }
+
+//    def 'test GetElementType' () {
+//        given:
+//        def isCollection = new ChangeableBoolean()
+//
+//        when:
+//        def field = CollectionService.class.getDeclaredField(fieldName);
+//        def fieldType = ClassHelper.getElementType(field.getType(), field.getGenericType(), isCollection)
+//
+//        then:
+//        field != null
+//        fieldType == type
+//        isCollection.get() == collection
+//
+//        where:
+//        fieldName   | type              | collection
+//        "listField" | String.class      | true
+//        "mapField"  | Integer.class     | true
+//        "mixField"  | Float.class       | true
+//        "mixField2" | Double.class      | true
+//        "test"      | FakeClass.class   | true
+//        "none"      | FakeClass.class   | false
+//    }
+
+    private interface IFakeInterface<T extends Annotation> { }
+
+    private final class FakeService implements IFakeInterface<TestAnno> { }
+
+    @SuppressWarnings("unused")
+    private final class CollectionService {
+
+        private List<String> listField;
+        private Map<String, Integer> mapField;
+        private Map<String, List<Float>> mixField;
+        private List<Map<String, Double>> mixField2;
+        private Map<String, FakeClass<String>> test;
+        private FakeClass<String> none;
+    }
+
+    private final class FakeClass<T> {}
+}
+
+public @interface TestAnno {}
