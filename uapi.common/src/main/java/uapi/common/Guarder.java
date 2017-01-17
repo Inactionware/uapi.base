e30faadf185101af3221830adc9c0c6be5f97c7c
xquan <xiaoming.quan@netscout.com>
2017-01-17 11:42:07 +0800

    Add Guarder to support uapi.service project

---
+++ uapi.common/src/main/java/uapi/common/Guarder.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (C) 2017. The UAPI Authors
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at the LICENSE file.
+ *
+ * You must gained the permission from the authors if you want to
+ * use the project into a commercial product
+ */
+
+package uapi.common;
+
+import java.util.concurrent.locks.Lock;
+
+/**
+ * A Guarder protected one or more statement by specific lock to make these statements
+ * are thread safe.
+ */
+public class Guarder {
+
+    /**
+     * Create a new Guarder by specific lock
+     *
+     * @param   lock
+     *          The lock used to protected one or more statement
+     * @return  The Guarder instance
+     */
+    public static Guarder by(final Lock lock) {
+        ArgumentChecker.notNull(lock, "lock");
+        return new Guarder(lock);
+    }
+
+    private final Lock _lock;
+
+    private Guarder(final Lock lock) {
+        this._lock = lock;
+    }
+
+    /**
+     * Run specific runnable statements
+     *
+     * @param   run
+     *          Some runnable statements
+     */
+    public void run(Runnable run) {
+        this._lock.lock();
+        try {
+            run.run();
+        } finally {
+            this._lock.unlock();
+        }
+    }
+
+    /**
+     * Run specific runnable statements and retrieve its result
+     * @param   run
+     *          Some runnable statements
+     * @param   <T>
+     *          The return type
+     * @return
+     */
+    public <T> T runForResult(Resultful<T> run) {
+        T rtn = null;
+        this._lock.lock();
+        try {
+            rtn = run.run();
+        } finally {
+            this._lock.unlock();
+        }
+        return rtn;
+    }
+}
---
+++ uapi.common/src/test/groovy/uapi/common/GuarderTest.groovy
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2017. The UAPI Authors
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at the LICENSE file.
+ *
+ * You must gained the permission from the authors if you want to
+ * use the project into a commercial product
+ */
+
+package uapi.common
+
+import spock.lang.Specification
+
+import java.util.concurrent.locks.Lock
+
+/**
+ * Unit test for Guarder
+ */
+class GuarderTest extends Specification {
+
+    def 'Test run'() {
+        given:
+        Lock lock = Mock(Lock)
+        final List<String> strs = new ArrayList<>()
+
+        when:
+        Guarder.by(lock).run({ strs.add("Hello"); })
+
+        then:
+        strs.size() == 1
+        strs.get(0) == "Hello"
+        1 * lock.lock()
+        1 * lock.unlock()
+    }
+
+    def 'Test run by result'() {
+        given:
+        Lock lock = Mock(Lock)
+
+        when:
+        String result = Guarder.by(lock).runForResult({ return "Hello"; })
+
+        then:
+        result == "Hello"
+        1 * lock.lock()
+        1 * lock.unlock()
+    }
+}
