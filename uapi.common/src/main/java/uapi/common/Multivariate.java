0fb365163526a9a159e7d2bd6be3676c91d47ae6
minjing <min_jing@aliyun.com>
2017-02-15 11:14:53 +0800

    #9 Add new class to holder multiple variables

---
+++ uapi.common/src/main/java/uapi/common/Multivariate.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2017. The UAPI Authors
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at the LICENSE file.
+ *
+ * You must gained the permission from the authors if you want to
+ * use the project into a commercial product
+ */
+
+package uapi.common;
+
+import uapi.rx.Looper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The Multivariate hold multiple variables
+ */
+public class Multivariate {
+
+    private final int _count;
+
+    private final Object[] _items;
+
+    public Multivariate(final int count) {
+        ArgumentChecker.checkInt(count, "count", 1, Integer.MAX_VALUE);
+        this._count = count;
+        this._items = new Object[count];
+    }
+
+    public void put(final int index, final Object item) {
+        ArgumentChecker.checkInt(index, "index", 0, this._count - 1);
+        this._items[index] = item;
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T> T get(final int index) {
+        ArgumentChecker.checkInt(index, "index", 0, this._count - 1);
+        return (T) this._items[index];
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T> T remove(final int index) {
+        ArgumentChecker.checkInt(index, "index", 0, this._count - 1);
+        T item = (T) this._items[index];
+        this._items[index] = null;
+        return item;
+    }
+
+    public void clear() {
+        Looper.on(this._items).foreachWithIndex((index, item) -> this._items[index] = null);
+    }
+}
---
+++ uapi.common/src/test/groovy/uapi/common/MultivariateTest.groovy
@@ -0,0 +1,96 @@
+/*
+ * Copyright (C) 2017. The UAPI Authors
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at the LICENSE file.
+ *
+ * You must gained the permission from the authors if you want to
+ * use the project into a commercial product
+ */
+
+package uapi.common
+
+import spock.lang.Specification
+import uapi.InvalidArgumentException
+
+/**
+ * Unit test for Multivariate
+ */
+class MultivariateTest extends Specification {
+
+    def 'Test create instance'() {
+        when:
+        new Multivariate(count)
+
+        then:
+        noExceptionThrown()
+
+        where:
+        count               | placeholder
+        1                   | null
+        10                  | null
+        10000               | null
+    }
+
+    def 'Test put and get item'() {
+        when:
+        def inst = new Multivariate(3)
+        inst.put(0, first)
+        inst.put(1, second)
+        inst.put(2, third)
+
+        then:
+        inst.get(0) == first
+        inst.get(1) == second
+        inst.get(2) == third
+
+        where:
+        first   | second    | third
+        '1'     | '2'       | '3'
+        '1'     | null      | '2'
+    }
+
+    def 'Test put item by incorrect index'() {
+        when:
+        def inst = new Multivariate(3)
+        inst.put(index, null)
+
+        then:
+        thrown(InvalidArgumentException)
+
+        where:
+        index   | plactholder
+        -1      | null
+        3       | null
+        10      | null
+        -3      | null
+    }
+
+    def 'Test get item by incorrect index'() {
+        when:
+        def inst = new Multivariate(3)
+        inst.get(index)
+
+        then:
+        thrown(InvalidArgumentException)
+
+        where:
+        index   | plactholder
+        -1      | null
+        3       | null
+        10      | null
+        -3      | null
+    }
+
+    def 'Test clear'() {
+        when:
+        def inst = new Multivariate(3)
+        inst.put(0, '1')
+        inst.put(2, '3')
+        inst.clear()
+
+        then:
+        inst.get(0) == null
+        inst.get(1) == null
+        inst.get(2) == null
+    }
+}
