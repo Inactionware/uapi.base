d031d1e2492f93829725ddc085a7dbc3c5d30bf4
minjing <min_jing@aliyun.com>
2018-11-25 16:48:21 +0800

    Add script to get configuration from remote repo and import to current project build script

---
+++ build.sh
@@ -0,0 +1,14 @@
+# Check out build configuration repo from remote
+rm -rf .config
+mkdir .config
+cd .config
+
+git init
+git remote add -f origin git@gitlab.com:Inactionware/configuration.git
+git config core.sparsecheckout true
+echo "uapi" >> .git/info/sparse-checkout
+git pull origin master
+
+# Run gradle build script
+cd ..
+./gradlew clean build
