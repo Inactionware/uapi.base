6f90fcaef69bc3ecf9541ac434b407a18d60424c
minjing <min_jing@aliyun.com>
2019-02-21 15:09:08 +0800

    #72 Move java env initialization script to setup-env.sh file

--- setup-env.sh
+++ setup-env.sh
@@ -1,11 +1,27 @@
 #! /bin/bash
 
-# echo -e "Host gitlab.com\n" >> ~/.ssh/config
-# echo -e "    HostName gitlab.com\n" >> ~/.ssh/config
-# echo -e "    User git\n" >> ~/.ssh/config
-# echo -e "    StrictHostKeyChecking no\n" >> ~/.ssh/config
+os=`uname -s`
+host=`hostname`
+javac=`command -v javac`
 
-# Setup variable
+if [ -z $javac ]
+then
+    # Setup variable
+    echo -e "Initialize JAVA environment......\c"
+    if [ $USER == "min" ] && [ $host == "min-vm-elementary" ]       # At Linux VM host development
+    then
+        export JAVA_HOME="/home/min/Dev/zuluJdk-8.0.192/"
+    elif [ $USER == "xxx" ]                                         # At MacOS host development
+    then
+        export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.8.0_181.jdk/Contents/Home"
+    else
+        echo "Unsupported user - $USER@$host, please reconfig the user"
+        return 1
+    fi
+    export PATH=$JAVA_HOME/bin:$PATH
+    echo "Done"
+fi
+
 cfgBranch=master # The configuration repo branch/tag name will be checked out
 
 # Check out build configuration repo from remote
@@ -20,5 +36,5 @@ echo "uapi" >> .git/info/sparse-checkout
 git checkout ${cfgBranch}
 
 # Run gradle build script
-# cd ..
+cd ..
 # ./gradlew clean build
