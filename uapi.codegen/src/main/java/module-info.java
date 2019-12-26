93ce01ecfa977e15cc9a25990109f594a9c724db
minjing <min_jing@aliyun.com>
2019-12-26 15:25:32 +0800

    Remove ASM library from dependency

--- uapi.codegen/src/main/java/module-info.java
+++ uapi.codegen/src/main/java/module-info.java
@@ -2,7 +2,6 @@ module uapi.codegen {
     requires java.compiler;
     requires freemarker;
     requires auto.service.annotations;
-    requires org.objectweb.asm;
     requires uapi.common;
 
     exports uapi.codegen;
